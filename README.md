fc9515a873a2b81f07564395325dad8eda97cb21
satackey <21271711+satackey@users.noreply.github.com>
2020-03-25 15:10:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# actions-exec-wrapper
+
+[@actions/exec](https://www.npmjs.com/package/@actions/exec) wrapper to get listener data value as return value.
+
+## Usage
+```js
+const exec = require('actions-exec-wrapper');
+
+const options = {};
+options.cwd = './lib';
+const { stdout: stdBuffer, stderr: errBuffer } = await exec.exec('node', ['index.js', 'foo=bar'], options)';
+const myOutput = stdBuffer.toString();
+const myError = errBuffer.toString();
+```
+
+### Using @actions/exec
+```js
+const exec = require('@actions/exec');
+
+let myOutput = '';
+let myError = '';
+
+const options = {};
+options.listeners = {
+  stdout: (data: Buffer) => {
+    myOutput += data.toString();
+  },
+  stderr: (data: Buffer) => {
+    myError += data.toString();
+  }
+};
+options.cwd = './lib';
+
+await exec.exec('node', ['index.js', 'foo=bar'], options);
+```
+Above code from [actions/toolkit repository](https://github.com/actions/toolkit/tree/master/packages/exec)
+
+## Install
+```shell
+$ npm install actions-exec-wrapper
+$ # or
+$ yarn add actions-exec-wrapper
+```
---
+++ index.ts
@@ -0,0 +1,48 @@
+const actionsExec = require('@actions/exec')
+import execInterfaces from '@actions/exec/lib/interfaces'
+
+const exec = async (command: string, args?: string[], options?: execInterfaces.ExecOptions) => {
+  let stdout = Buffer.concat([], 0)
+  let stderr = Buffer.concat([], 0)
+  let stdline = ''
+  let errline = ''
+  let debug = ''
+
+  const listeners = {
+    stdout: (data: Buffer) => {
+      const concatData = [stdout, data]
+      const concatLength = stdout.length + data.length
+      stdout = Buffer.concat(concatData, concatLength)
+    },
+    stderr: (data: Buffer) => {
+      const concatData = [stderr, data]
+      const concatLength = stderr.length + data.length
+      stderr = Buffer.concat(concatData, concatLength)
+    },
+    stdline: (data: Buffer) => {
+      stdline += data.toString()
+    },
+    errline: (data: Buffer) => {
+      stdline += data.toString()
+    },
+    debug: (data: Buffer) => {
+      stdline += data.toString()
+    }
+  }
+
+  const exitCode = await actionsExec.exec(command, args, {
+    listeners,
+    ...options,
+  })
+
+  return {
+    exitCode,
+    stdout,
+    stderr,
+    stdline,
+    errline,
+    debug,
+  }
+}
+
+export default exec
